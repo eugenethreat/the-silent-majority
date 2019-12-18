cae65a64f5bc8bb5bceeff8702cd1fcaff9f0675
unknown <eugryooene@gmail.com>
2019-12-18 08:50:59 -0500

    initial commit

---
+++ src/main/java/org/themajoritytwitterbot/App.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.themajoritytwitterbot;
+
+/**
+ *
+ * @author eugene
+ */
+public class App {
+    
+    public static void main(String args[]){
+        
+        /*
+        generates tweets in the format "the ___ majority", where ____ is a random adjective. 
+        
+        uses Twitter4J
+        
+        wordlist from https://gist.github.com/hugsy/8910dc78d208e40de42deb29e62df913#file-english-adjectives-txt 
+        */
+        
+        MajorityTwitterBot tb = new MajorityTwitterBot();
+        
+        
+        
+    }
+    
+}
